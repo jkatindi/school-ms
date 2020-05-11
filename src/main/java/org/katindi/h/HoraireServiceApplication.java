a2120666d4daf1e9d4a359285ddf27d75891eb54
jean katindi <jeankatindi@yahoo.fr>
2020-05-11 18:36:23 +0200

    first commit

---
+++ src/main/java/org/katindi/h/HoraireServiceApplication.java
@@ -0,0 +1,13 @@
+package org.katindi.h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraireServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraireServiceApplication.class, args);
+	}
+
+}
